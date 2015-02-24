e245df942fe1d0cfe1eda2e8a24ccc9ad664fb5c
hjorthjort <rikard.hjort@gmail.com>
2015-02-24 13:55:17 +0100

    Creates starting main class, which sets System Look and Feel. Initializes TopFrame class

--- i_mat/src/i_mat/IMat.java
+++ i_mat/src/i_mat/IMat.java
@@ -5,6 +5,9 @@
  */
 package i_mat;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Hjort
@@ -15,7 +18,25 @@ public class IMat {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            //Set look and feel to system standard
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException | ClassNotFoundException | 
+                InstantiationException | IllegalAccessException e) {
+            System.out.println(UIManager.getSystemLookAndFeelClassName() + 
+                    " look and feel not supported");
+        }
+        
+        new TopFrame().setVisible(true);
+        
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //This code will run before shutdown
+                System.out.println("Closing RecipeSearch");
+            }
+        }));
     }
     
 }
