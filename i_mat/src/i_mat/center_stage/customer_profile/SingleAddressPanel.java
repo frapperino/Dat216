46d60632f15704ee63d9622b1cfc5d863d59f984
weeeeeew <jackpettersson@gmail.com>
2015-02-28 12:05:45 +0100

    Started work on dynamic delivery addresses

--- i_mat/src/i_mat/center_stage/customer_profile/SingleAddressPanel.java
+++ i_mat/src/i_mat/center_stage/customer_profile/SingleAddressPanel.java
@@ -5,19 +5,29 @@
  */
 package i_mat.center_stage.customer_profile;
 
+import i_mat.model.Model;
+import se.chalmers.ait.dat215.project.Customer;
+
 /**
  *
  * @author weeeeeew
  */
 public class SingleAddressPanel extends javax.swing.JPanel {
 
+    private Customer customer;
+    
     /**
      * Creates new form SingleAddress
      */
     public SingleAddressPanel() {
-        initComponents();
+        this(Model.getTestCustomer());
     }
 
+    public SingleAddressPanel(Customer c) {
+        initComponents();
+        customer = c;
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,19 +37,28 @@ public class SingleAddressPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel1.setText("jLabel1");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(0, 355, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(0, 284, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
