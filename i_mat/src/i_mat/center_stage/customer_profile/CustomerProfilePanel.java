36c1dfffb2f1d87e2dfbc9900ed5ffa24c0f8e08
weeeeeew <jackpettersson@gmail.com>
2015-02-28 17:11:11 +0100

    Fixed painting of delivery addresses

--- i_mat/src/i_mat/center_stage/customer_profile/CustomerProfilePanel.java
+++ i_mat/src/i_mat/center_stage/customer_profile/CustomerProfilePanel.java
@@ -27,19 +27,8 @@ public class CustomerProfilePanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        addressesPanel1 = new i_mat.center_stage.customer_profile.AddressesPanel();
         paymentsPanel1 = new i_mat.center_stage.customer_profile.PaymentsPanel();
-
-        javax.swing.GroupLayout addressesPanel1Layout = new javax.swing.GroupLayout(addressesPanel1);
-        addressesPanel1.setLayout(addressesPanel1Layout);
-        addressesPanel1Layout.setHorizontalGroup(
-            addressesPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        addressesPanel1Layout.setVerticalGroup(
-            addressesPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
+        addressesPanel2 = new i_mat.center_stage.customer_profile.AddressesPanel();
 
         javax.swing.GroupLayout paymentsPanel1Layout = new javax.swing.GroupLayout(paymentsPanel1);
         paymentsPanel1.setLayout(paymentsPanel1Layout);
@@ -56,22 +45,24 @@ public class CustomerProfilePanel extends javax.swing.JPanel {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(addressesPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(paymentsPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(addressesPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(addressesPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
+                .addComponent(addressesPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(paymentsPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 82, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private i_mat.center_stage.customer_profile.AddressesPanel addressesPanel1;
+    private i_mat.center_stage.customer_profile.AddressesPanel addressesPanel2;
     private i_mat.center_stage.customer_profile.PaymentsPanel paymentsPanel1;
     // End of variables declaration//GEN-END:variables
 }
